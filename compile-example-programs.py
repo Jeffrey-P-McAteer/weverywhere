feece56df6f4ac9e9c98bced5a405bcc87dd3563
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-11-10 10:46:31 -0500

    misc

--- compile-example-programs.py
+++ compile-example-programs.py
@@ -10,13 +10,57 @@ import sys
 import subprocess
 import shutil
 import traceback
+import shlex
 
 REPO_DIR = os.path.dirname(__file__)
 
+def check_req_bins():
+  req_bins = [
+    ('zig', 'Zig performs compilation of C code to .wasm executable files.')
+  ]
+  missing_one = False
+  for b, reason_txt in req_bins:
+    if not shutil.which(b):
+      print(f'The required program "{b}" is mising on your system! This is required because: {reason_txt}')
+      missing_one = True
+  if missing_one:
+    raise Exception(f'At least one required program is missing! Ensure it is installed and on your PATH.')
+
+def read_compile_command_from_c_source(c_source_file):
+  # Default
+  compile_cmd = [
+    'zig', 'cc',
+      '-target', 'wasm32-wasi',
+      '-O2',
+      '-o', 'OUT_FILE',
+      'THIS_FILE'
+  ]
+  with open(c_source_file, 'r', encoding='UTF-8') as fd:
+    while line := fd.readline():
+      if line.startswith('// COMPILE:'):
+        compile_cmd = shlex.split( line[len('// COMPILE:'):].strip() )
+  return compile_cmd
+
+def template_compile_command(cmd, template_dict):
+  return [ template_dict.get(c, c) for c in cmd ]
+
+def run_cmd(cmd):
+  print(f'> {" ".join(cmd)}')
+  subprocess.run(cmd, check=True)
+
 def compile_example_program(example_path, out_dir):
   print(f'[ compile ] {os.path.relpath(example_path, REPO_DIR)}')
+
+  example_path_stem = os.path.splitext(os.path.basename(example_path))[0] # name w/o extension
+  compile_cmd_dict = {
+    'THIS_FILE': example_path,
+    'OUT_FILE': os.path.join(out_dir, f'{example_path_stem}.wasm'),
+  }
+
   if example_path.endswith('.c'):
-    pass
+    bare_cmd = read_compile_command_from_c_source(example_path)
+    cmd = template_compile_command(bare_cmd, compile_cmd_dict)
+    run_cmd(cmd)
   else:
     raise Exception(f'[ compile_example_program ] We do not know how to compile the example program at {example_path}! Please add an implementation to compile_example_program().')
 
--- example-programs/report-sysinfo.c
+++ example-programs/report-sysinfo.c
@@ -1,2 +1,26 @@
 
-int
+// COMPILE: zig cc -Os -target wasm32-wasi -mexec-model=reactor THIS_FILE -o OUT_FILE
+
+
+// TODO lotsa design work?
+
+__attribute__((export_name("do_report")))
+int do_report(char* out, unsigned int out_len) {
+
+  if (out_len > 10) {
+    out[0] = 'H';
+    out[1] = 'e';
+    out[2] = 'l';
+    out[3] = 'l';
+    out[4] = 'o';
+    out[5] = ' ';
+    out[6] = 'W';
+    out[7] = 'o';
+    out[8] = 'r';
+    out[9] = 'l';
+    out[10] = 'd';
+    out[11] = '\0';
+  }
+
+  return 0;
+}
