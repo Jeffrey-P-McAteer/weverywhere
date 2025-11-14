05ad8a527f824d43e7cea20ca1a22f637d7ea728
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-11-14 14:28:19 -0500

    misc

--- src/comm.rs
+++ src/comm.rs
@@ -5,7 +5,12 @@
 /// for communication purposes.
 pub struct Terminal {
   pub human_name: String,
+
+  pub human_name_utf8sig: Vec<u8>,
+
   pub pub_key: Vec<u8>,
+
+  pub connection: Box<dyn Connectable>,
 
 }
 
@@ -26,3 +31,19 @@ pub struct Ship {
 pub struct Lake {
   pub ships: Vec<Ship>,
 }
+
+
+/// This trait allows us to implement TCP/UDP/Socket connections later without needing to
+/// hoist storage for those connections in the comm structs defined here.
+pub trait Connectable {
+   fn connect(&mut self) -> Result<(), Box<dyn std::error::Error>>;
+
+   fn is_connected(&mut self) -> Result<bool, Box<dyn std::error::Error>>;
+
+   fn tx_encoded(&mut self, message: &[u8]) -> Result<(), Box<dyn std::error::Error>>;
+   fn rx_encoded(&mut self) -> Result<(), Box<dyn std::error::Error>>;
+
+   fn disconnect(&mut self) -> Result<(), Box<dyn std::error::Error>>;
+
+}
+
