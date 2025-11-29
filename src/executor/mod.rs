7a7f804f8429b49ef63705fd3878160eac481cf1
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-11-29 12:14:50 -0500

    structs++

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -2,14 +2,23 @@
 use crate::*;
 use crate::args::*;
 
+/**
+ * Stores all data for the Executor.
+ **/
 pub struct Executor {
 
   next_pid: std::sync::atomic::AtomicU64,
+
+  untrusted_allowed_instructions: std::sync::atomic::AtomicU64,
+
+  trusted_allowed_instructions: std::sync::atomic::AtomicU64,
 
   /// Every program submited will get a unique number (PID) and RunningProgram entry here.
   running_programs: dashmap::DashMap<u64, RunningProgram>,
 
   trusted_keys: dashmap::DashMap<String, ed25519_dalek::VerifyingKey>,
+
+
 }
 
 pub struct Program {
@@ -24,6 +33,10 @@ impl Executor {
   pub fn new() -> Executor {
     Executor {
       next_pid: std::sync::atomic::AtomicU64::new(0),
+
+      untrusted_allowed_instructions: std::sync::atomic::AtomicU64::new(16 * 1024),
+
+      trusted_allowed_instructions: std::sync::atomic::AtomicU64::new(u64::MAX),
 
       // We use a high shard count (128) here on the expectation that many processes will be running in parallel,
       // and we want to enable lots of write capacity. This is a similar reason as why we have a large capacity up-front.
